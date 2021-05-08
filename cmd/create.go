efb6437f3002611d165144f5ade1f3ac31d149ff
p0tr3c <p0tr3c@protonmail.com>
2021-05-08 10:27:22 +0100

    feat: boilerplate workspace command
    Base structure for workspace command
    workspace command will be responsible for creating
    terragrunt workspace structure.
    This includes directory structure, config templates and
    github action workflows

--- cmd/create.go
+++ cmd/create.go
@@ -11,5 +11,18 @@ func NewCreateCommand(in io.Reader, out, outerr io.Writer) *cobra.Command {
 		Short: "Creates terra-ci resource type",
 		Run:   runHelp,
 	}
+	command.AddCommand(NewCreateWorkspaceCommand(in, out, outerr))
 	return command
+}
+
+func NewCreateWorkspaceCommand(in io.Reader, out, outerr io.Writer) *cobra.Command {
+	command := &cobra.Command{
+		Use:   "workspace",
+		Short: "Creates new terragrunt workspace",
+		Run:   runCreateWorkspace,
+	}
+	return command
+}
+
+func runCreateWorkspace(cmd *cobra.Command, args []string) {
 }
